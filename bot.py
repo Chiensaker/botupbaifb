da5034b15c3a9ed24076ffb1cefdd1fb0691fcd1
Chiensaker <102570048+Chiensaker@users.noreply.github.com>
2025-11-19 19:18:33 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,11 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
 # --- CẤU HÌNH ---
-TELEGRAM_TOKEN = os.environ.get(8434809055:AAEO6AQoRUqN4cSpOppfSGOJHoOPF9NuwxM)
-PAGE_ID = os.environ.get(584599948078305)
-PAGE_ACCESS_TOKEN = os.environ.get(EAAVmnqZCromIBP66TB5asI3nDuAkTZAGqYfUjYuIPR5mCl3J5sfZBueF8g55UDlnCI3ch58VxEPbQ5ZCYykncFySKGI5OV3dsKf1qcOmNT1LHpC03XV1RfhMOVT7q7AX1xBmKPeC9HnVb569ddPjivkjmvLQbgro0UQ4p3UZBWoZAtIZAYr8yBSel5eC64ULcj8epsH)
+# --- CẤU HÌNH ---
+# Chỉ gọi TÊN BIẾN, không điền giá trị vào đây
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+PAGE_ID = os.environ.get("PAGE_ID")
+PAGE_ACCESS_TOKEN = os.environ.get("PAGE_ACCESS_TOKEN")
 
 album_storage = {}
 
@@ -105,4 +107,5 @@ if __name__ == '__main__':
     print("Bot khoi dong...")
 
     app_bot.run_polling()
+
 
